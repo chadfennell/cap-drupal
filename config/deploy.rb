8e3535757499584274c94455d9baf6cc38eb17c3
Chad Fennell <libsys@gmail.com>
2011-12-22 13:18:36 -0600

    Allow :db role to grab some config information from the :web role - useful if your database resides on a seperate server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,8 @@
 ### This file contains project-specific settings ###
 
+# Use a remote cache to speed things up
+# set :deploy_via, :remote_cache
+
 # Multistage support - see config/deploy/[STAGE].rb for specific configs
 set :default_stage, "development"
 set :stages, %w(production development staging)
@@ -10,21 +13,7 @@ set :use_sudo, false
 # Some configs live in JSON to make them portable between PHP and Ruby/Cap
 require 'json'
 
-# In many cases, you might have a data mount, so provide a single default place
-# to store database backups
-_cset(:current_release_stamp) { current_release.split("/").pop }
 _cset(:db_backup_files) { capture("ls -x #{db_backup_dir}", :except => { :no_release => true }).split.sort }
 _cset(:db_conf_dir) { File.join(shared_path, "conf") }
-_cset(:db_settings_spare) { which_database("spare") }
-_cset(:db_settings_live) { which_database("live") }
+_cset(:current_release_stamp) { current_release.split("/").pop }
 
-def which_database(db)
-  run "cat #{shared_path}/default/db_settings.json" do |channel, stream, data|
-    info = JSON.parse(data)
-    if db == "spare"
-      return File.join(db_conf_dir, info["database_spare"])
-    else
-      return File.join(db_conf_dir, info["database"])
-    end
-  end
-end
